174707c27a29ece3419567cef2b0db841cb7fa5e
Minh Quan <minhquan862004@gmail.com>
2024-02-16 20:23:31 +0200

    Render posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Axios from "axios";
 import { useState, useEffect } from "react";
 import PostCard from "./PostCard";
 import userAvatar1 from "../../assets/userAvatar1.jpg";
@@ -8,42 +9,17 @@ import post2Image from "../../assets/post2.jpg";
 
 
 const Post = () => {
-    const posts = [{
-        "post_id": 1, 
-        "user_avatar": userAvatar1,
-        "name": "Pietro Bolcato",
-        "description": "AI Engineer @Kittl | Gen AI, NLP, CV | MLOps | MSc AI, double degree from KTH 🇸🇪 and TU Berlin 🇩🇪 | 2x Azure AI certified",
-        "created_date": "2022-03-25",
-        "post_content": "🔥 BLIVA: A Multimodal Large Language Model for Better Handling of Text-Rich Visual Questions. In this study, the researchers introduce BLIVA (InstructBLIP with Visual Assistant), a multimodal LLM designed to bridge this gap. BLIVA combines learned query embeddings closely linked to the LLM and image-encoded patch embeddings containing rich image data. This approach enhances text-image comprehension, surpassing previous methods. The model undergoes two-stage training, involving pre-training the patch embeddings projection layer and fine-tuning the Q-former and patch embeddings projection layer using instruction tuning data. BLIVA significantly impacts visual question-answering tasks, particularly in scenarios requiring detailed captions and small captions combined with VQA. It offers a novel approach to multimodal AI, improving text-image understanding.",
-        "post_image": post1Image,
-        "like": 99,
-        "comment": 0,
-        "repost": 0
-    }, {
-        "post_id": 2,
-        "user_avatar": userAvatar2,
-        "name": "Anna Bertoldini",
-        "description" : "Global Head of Employer Branding at NIQ | Frontify Top 30 Brand Visionaries 2023 | Human Storyteller | Personal Branding Expert | Keynote Speaker",
-        "created_date": "2023-07-30",
-        "post_content": "You’re doing just fine. Keep going.",
-        "post_image": post2Image,
-        "like": 121,
-        "comment": 0,
-        "repost": 0
-    }]
-
-    // Set data to State of component
-    const [post, setPost] = useState(() => {
-        const storedPosts = localStorage.getItem("post")
-        return storedPosts ? JSON.parse(storedPosts) : posts
-    })
+    const [post, setPost] = useState([{}])
 
     useEffect(() => {
-        localStorage.setItem('post', JSON.stringify(post))
-        localStorage.setItem('total post', post.length)
+        Axios.get("http://localhost:5000/posts").then(
+            response => { 
+                setPost(response.data)
+                console.log(post)
+            }
+        )
     }, [])
 
-    // Loop through array and render all posts
     let postCards = [];
      
     for (var i = 0; i < post.length; i++) {
