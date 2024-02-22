9770c77e47528c0bba72cc28a621365dab0a7200
Minh Quan <minhquan862004@gmail.com>
2024-02-22 18:35:14 +0200

    Render dates

--- src/components/post/PostCard.jsx
+++ src/components/post/PostCard.jsx
@@ -169,10 +169,6 @@ const PostCard = ({ postCard, setPostCard }) => {
         setShowCommentInput(true)
     }
 
-    const date = new Date();
-    const postDate = new Date(postCard.created_date);
-    const diffTime = new Date(date - postDate);
-
     return(
         <div className="share-feed">
             <div className="user-menu-container">
@@ -181,7 +177,7 @@ const PostCard = ({ postCard, setPostCard }) => {
                     <div className="user-name-description">
                         <span className="user-name">{postCard.username}</span>
                         <span className="user-description">{postCard.description}</span>
-                        <span className="post-date">{diffTime.getMonth()}m
+                        <span className="post-date">{postCard.created_date}
                             <span style={{margin: '4px', color: '#00000099'}}>•</span>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" class="mercado-match" width="16" height="16" focusable="false">
                                 <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z"></path>
